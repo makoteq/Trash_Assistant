1d36343ba7a36c34682797008b345063437953e0
GRZ4NA <grzanatv@gmail.com>
2021-12-08 21:59:26 +0100

    fixed typescript

--- src/components/Ai/index.tsx
+++ src/components/Ai/index.tsx
@@ -1,107 +1,121 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, LegacyRef } from "react";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function Ai() {
-    const [isModelLoading, setIsModelLoading] = useState(false)
-    const [model, setModel] = useState(null)
-    const [imageURL, setImageURL] = useState(null);
-    const [results, setResults] = useState([])
-    const [history, setHistory] = useState([])
+    const [isModelLoading, setIsModelLoading] = useState(false);
+    const [model, setModel] = useState<mobilenet.MobileNet>();
+    const [imageURL, setImageURL] = useState<string>();
+    const [results, setResults] = useState<any>([]);
+    const [history, setHistory] = useState<any[]>([]);
 
-    const imageRef = useRef()
-    const textInputRef = useRef()
-    const fileInputRef = useRef()
+    const imageRef = useRef<HTMLImageElement>();
+    const textInputRef = useRef<{ value: string }>();
+    const fileInputRef = useRef<HTMLElement>();
 
     const loadModel = async () => {
-        setIsModelLoading(true)
+        setIsModelLoading(true);
         try {
-            const model = await mobilenet.load()
-            setModel(model)
-            setIsModelLoading(false)
+            const model = await mobilenet.load();
+            setModel(model);
+            setIsModelLoading(false);
         } catch (error) {
-            console.log(error)
-            setIsModelLoading(false)
+            console.log(error);
+            setIsModelLoading(false);
         }
-    }
+    };
 
-    const uploadImage = (e) => {
-        const { files } = e.target
+    const uploadImage = (e: any) => {
+        const { files } = e.target;
         if (files.length > 0) {
-            const url = URL.createObjectURL(files[0])
-            setImageURL(url)
+            const url = URL.createObjectURL(files[0]);
+            setImageURL(url);
         } else {
-            setImageURL(null)
+            setImageURL("");
         }
-    }
+    };
 
     const identify = async () => {
-        textInputRef.current.value = ''
-        const results = await model.classify(imageRef.current)
-        setResults(results)
-    }
+        if (textInputRef.current && imageRef.current) {
+            textInputRef.current.value = "";
+            const results = await model?.classify(imageRef.current);
+            setResults(results);
+        }
+    };
 
-    const handleOnChange = (e) => {
-        setImageURL(e.target.value)
-        setResults([])
-    }
+    const handleOnChange = (e: any) => {
+        setImageURL(e.target.value);
+        setResults([]);
+    };
 
     const triggerUpload = () => {
-        fileInputRef.current.click()
-    }
+        if (fileInputRef.current) fileInputRef.current.click();
+    };
 
     useEffect(() => {
-        loadModel()
-    }, [])
+        loadModel();
+    }, []);
 
     useEffect(() => {
         if (imageURL) {
-            setHistory([imageURL, ...history])
+            setHistory([imageURL, ...history]);
         }
-    }, [imageURL])
+    }, [imageURL]);
 
     if (isModelLoading) {
-        return <h2>Model Loading...</h2>
+        return <h2>Model Loading...</h2>;
     }
 
     return (
         <div className="App">
-            <h1 className='header'>Image Identification</h1>
-            <div className='inputHolder'>
-                <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
-                <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
-                <span className='or'>OR</span>
-                <input type="text" placeholder='Paster image URL' ref={textInputRef} onChange={handleOnChange} />
+            <h1 className="header">Image Identification</h1>
+            <div className="inputHolder">
+                <input type="file" accept="image/*" capture="camera" className="uploadInput" onChange={uploadImage} ref={fileInputRef as LegacyRef<HTMLInputElement>} />
+                <button className="uploadImage" onClick={triggerUpload}>
+                    Upload Image
+                </button>
+                <span className="or">OR</span>
+                <input type="text" placeholder="Paster image URL" ref={textInputRef as LegacyRef<HTMLInputElement>} onChange={handleOnChange} />
             </div>
             <div className="mainWrapper">
                 <div className="mainContent">
                     <div className="imageHolder">
-                        {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} />}
+                        {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef as LegacyRef<HTMLImageElement> | undefined} />}
                     </div>
-                    {results.length > 0 && <div className='resultsHolder'>
-                        {results.map((result, index) => {
-                            return (
-                                <div className='result' key={result.className}>
-                                    <span className='name'>{result.className}</span>
-                                    <span className='confidence'>Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className='bestGuess'>Best Guess</span>}</span>
-                                </div>
-                            )
-                        })}
-                    </div>}
+                    {results.length > 0 && (
+                        <div className="resultsHolder">
+                            {results.map((result: { className: string; probability: number }, index: number) => {
+                                return (
+                                    <div className="result" key={result.className}>
+                                        <span className="name">{result.className}</span>
+                                        <span className="confidence">
+                                            Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className="bestGuess">Best Guess</span>}
+                                        </span>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    )}
                 </div>
-                {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
+                {imageURL && (
+                    <button className="button" onClick={identify}>
+                        Identify Image
+                    </button>
+                )}
             </div>
-            {history.length > 0 && <div className="recentPredictions">
-                <h2>Recent Images</h2>
-                <div className="recentImages">
-                    {history.map((image, index) => {
-                        return (
-                            <div className="recentPrediction" key={`${image}${index}`}>
-                                <img src={image} alt='Recent Prediction' onClick={() => setImageURL(image)} />
-                            </div>
-                        )
-                    })}
+            {history.length > 0 && (
+                <div className="recentPredictions">
+                    <h2>Recent Images</h2>
+                    <div className="recentImages">
+                        {history.map((image, index) => {
+                            return (
+                                <div className="recentPrediction" key={`${image}${index}`}>
+                                    <img src={image} alt="Recent Prediction" onClick={() => setImageURL(image)} />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>}
+            )}
         </div>
     );
 }
