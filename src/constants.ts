af354f3ef6238162ff63b8466ddaf1a2f5b3c3ab
GRZ4NA <grzanatv@gmail.com>
2021-12-10 20:12:21 +0100

    code tweaks and optimization

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,13 @@ import { Context, createContext } from "react";
 
 export const videoStreamCtx = createContext(null) as Context<MediaStream | null>;
 export const modelCtx = createContext(null) as Context<MobileNet | null>;
+
+//0-papier,1-metale i tworzywa sztuczne,2-szkło,3-bio,4-resztkowe
+export const database = [
+    { AIname: "water bottle", type: 1 },
+    { AIname: "cash machine", type: 0 },
+    { AIname: "iPod", type: 0 },
+    { AIname: "cash machine", type: 0 },
+    { AIname: "cash machine", type: 0 },
+    { AIname: "cash machine", type: 0 },
+];
