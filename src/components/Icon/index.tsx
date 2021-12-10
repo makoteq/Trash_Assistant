b360d412e1b4e2a0e44d7fb260e522f3694517d5
GRZ4NA <grzanatv@gmail.com>
2021-12-11 00:31:09 +0100

    icon classname

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
-export const Icon: FC<{ type: string; color?: string; size?: number }> = (props) => {
-    return <i style={{ color: props.color ?? "inherit", fontSize: props.size ? `${props.size}px` : "inherit" }} className={`bi bi-${props.type}`}></i>;
+export const Icon: FC<{ type: string; color?: string; size?: number; className?: string }> = (props) => {
+    return <i style={{ color: props.color ?? "inherit", fontSize: props.size ? `${props.size}px` : "inherit" }} className={`bi bi-${props.type} ${props.className ?? ""}`}></i>;
 };
