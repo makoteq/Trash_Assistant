6d4e56b8465a887c657d43ce80c2c63cc8456e92
GRZ4NA <grzanatv@gmail.com>
2021-12-10 18:44:41 +0100

    added alt to remove warning

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -8,7 +8,7 @@ import { Box } from "../Box";
 export const Results: FC<{}> = (_) => {
     const [el, uel] = useState(
         <Box direction="column" gap={10} verticalAlignment="center" horizontalAlignment="center">
-            <img className={style.loadingCircle} src={loadingCircle} width={100} height={100} />
+            <img alt="loading-circle" className={style.loadingCircle} src={loadingCircle} width={100} height={100} />
             <h1 className={style.loadingLbl}>Trwa analizowanie zdjęcia...</h1>
         </Box>
     );
