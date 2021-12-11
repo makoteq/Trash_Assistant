eb36735bc0b4c6e80ef989c98f84876299a7e4a0
Maciej Kotowski <makoteq@gmail.com>
2021-12-11 15:32:06 +0100

    fonts & style

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -6,40 +6,63 @@ import loadingCircle from "../../assets/loadingCircle.png";
 import { Box } from "../Box";
 
 export const Results: FC<{}> = (_) => {
-    const [el, uel] = useState(
-        <Box direction="column" gap={10} verticalAlignment="center" horizontalAlignment="center">
-            <img alt="loading-circle" className={style.loadingCircle} src={loadingCircle} width={100} height={100} />
-            <h1 className={style.loadingLbl}>Trwa analizowanie zdjęcia...</h1>
-        </Box>
+  const [el, uel] = useState(
+    <Box
+      direction="column"
+      gap={10}
+      verticalAlignment="center"
+      horizontalAlignment="center"
+    >
+      <img
+        alt="loading-circle"
+        className={style.loadingCircle}
+        src={loadingCircle}
+        width={100}
+        height={100}
+      />
+      <h1 className={style.loadingLbl}>Trwa analizowanie zdjęcia...</h1>
+    </Box>
+  );
+  const model = useContext(modelCtx);
+  const getBestGuess = (g: string) => g.split(",")[0];
+  const container = {
+    padding: "20px",
+    height: "60vh",
+    minWidth: "40vw",
+    maxWidth: "80vw",
+    borderRadius: "10px",
+    color: "#000",
+    fontWeight: 700,
+    fontSize: "1.8em",
+  };
+  useEffect(() => {
+    const can = getVideoFrame(
+      document.querySelector("#video-feed") as HTMLVideoElement
     );
-    const model = useContext(modelCtx);
-    const getBestGuess = (g: string) => g.split(",")[0];
+    model?.classify(can).then((r) => {
+      console.log(getBestGuess(r?.[0].className));
+      database.every((element) => {
+        console.log(element.AIname);
+        if (element.AIname === getBestGuess(r?.[0].className)) {
+          console.log(element.type);
+          uel(
+            <>
+              <div style={container} className={element.type}>
+                <p>{element.type}</p>
+              </div>
+            </>
+          );
+          return false;
+        }
+        uel(
+          <>
+            <p style={container}>nie znaleziono</p>
+          </>
+        );
+        return true;
+      });
+    });
+  }, [model]);
 
-    useEffect(() => {
-        const can = getVideoFrame(document.querySelector("#video-feed") as HTMLVideoElement);
-        model?.classify(can).then((r) => {
-            console.log(getBestGuess(r?.[0].className));
-            database.every((element) => {
-                console.log(element.AIname);
-                if (element.AIname === getBestGuess(r?.[0].className)) {
-                    console.log(element.type);
-                    uel(
-                      <>
-                        <p className={element.type}>{element.type}</p>
-                      </>
-                  );
-                    return false;
-                }
-                uel(
-                  <>
-                    <p >nie znaleziono</p>
-                  </>
-              );
-                return true;
-            });
-       
-        });
-    }, [model]);
-
-    return el;
+  return el;
 };
--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,10 @@ export const modelCtx = createContext(null) as Context<MobileNet | null>;
 
 //0-papier,1-metale i tworzywa sztuczne,2-szkło,3-bio,4-resztkowe
 export const database = [
-    { AIname: "water bottle", type: "paper" },
-    { AIname: "cash machine", type: "paper" },
-    { AIname: "iPod", type: "paper" },
-    { AIname: "cash machine", type: "paper" },
-    { AIname: "cash machine", type: "paper" },
-    { AIname: "cash machine", type: "paper" },
+    { AIname: "water bottle", type: "_0" },
+    { AIname: "cash machine", type: "_1" },
+    { AIname: "iPod", type: "_2" },
+    { AIname: "cash machine", type: "_3" },
+    { AIname: "cash machine", type: "_4" },
+    { AIname: "cash machine", type: "_5" },
 ];
