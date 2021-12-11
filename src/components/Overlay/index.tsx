407ebad04c5b573a98f0fffa6cfe6e32d9445c31
Maciej Kotowski <makoteq@gmail.com>
2021-12-11 22:48:28 +0100

    alert

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -22,6 +22,7 @@ export const Overlay: FC = () => {
                     const guess = modelResult?.[0].className.split(",")[0] ?? "";
                     const result = database.find((e) => e.AIname.indexOf(guess) !== -1);
                     console.log(guess);
+                    alert(guess);
                     spawnDialog(
                         (c) => {
                             return (
