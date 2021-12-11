6e44753c66d587f0244b57696a50ca5f89ab966c
Maciej Kotowski <makoteq@gmail.com>
2021-12-11 22:40:41 +0100

    PSZOK

--- src/constants.ts
+++ src/constants.ts
@@ -8,11 +8,13 @@ export const modelCtx = createContext(null) as Context<MobileNet | null>;
 //0-papier,1-metale i tworzywa sztuczne,2-szkło,3-bio,4-resztkowe
 export const database: DatabaseRecord[] = [
     { AIname: ["paper"], type: "Papier", color: "#383087", colorName: "Niebieski" },
-    { AIname: ["water bottle", "pill bottle"], type: "Tworzywa sztuczne", color: "rgb(255,255,0)", colorName: "Żółty" },
+    { AIname: ["water bottle", "pill bottle","measuring cup","water jug","hair spray"], type: "Tworzywa sztuczne i Metale", color: "rgb(255,255,0)", colorName: "Żółty" },
     { AIname: [], type: "Metale", color: "#E31E25", colorName: "Czerwony" },
-    { AIname: ["iPod", "pop bottle"], type: "Szkło", color: "#009746", colorName: "Zielony" },
+    { AIname: ["", "pop bottle"], type: "Szkło", color: "#009746", colorName: "Zielony" },
     { AIname: [], type: "Bio", color: "#8F7459", colorName: "Brązowy" },
-    { AIname: [], type: "Zmieszane", color: "#2C2928", colorName: "Czarny" },
+    { AIname: ["lighter"], type: "Zmieszane", color: "#2C2928", colorName: "Czarny" },
+    { AIname: ["cellular telephone,iPod"], type: "PSZOK", color: "#2C2928", colorName: "Czarny" },
+
 ];
 
 // Global constants
