c00e7a0eebf909279ff18bb6d3e0051c55290533
Maciej Kotowski <makoteq@gmail.com>
2021-12-12 15:07:01 +0100

    change results description

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -5,26 +5,55 @@ import { Box } from "../Box";
 import { Icon } from "../Icon";
 
 export const Results: FC<{ type?: DatabaseRecord; ai?: string }> = (props) => {
-    return props.type ? (
-        <Box direction="column" gap={10} verticalAlignment="center" horizontalAlignment="center">
-            <h1 className={style.type} style={{ color: props.type?.color }}>
-                {props.type?.type}
-            </h1>
-            <p className={style.color} style={{ color: props.type?.color }}>
-                Kolor pojemnika: <b>{props.type?.colorName}</b>
-            </p>
-            <Box direction="column" verticalAlignment="center">
-                <Icon className={style.arrow} type="arrow-down" color={props.type?.color} size={90} />
-                <Icon className={style.trash} type="trash" color={props.type?.color} size={120} />
-            </Box>
-        </Box>
-    ) : (
-        <Box direction="column" gap={10} verticalAlignment="center" horizontalAlignment="center">
-            <Icon className={style.trash} type="x-circle" color={"#ff0000"} size={120} />
-            <h1 className={style.color} style={{ color: "#ff0000" }}>
-                Niestety nie udało się rozpoznać typu odpadu
-            </h1>
-            {props.ai && <p style={{ color: "#666666", fontSize: "20px", fontWeight: "normal" }}>Znalezione dopasowanie: {props.ai}</p>}
-        </Box>
-    );
+  return props.type ? (
+    <Box
+      direction="column"
+      gap={10}
+      verticalAlignment="center"
+      horizontalAlignment="center"
+    >
+      <h1 className={style.type} style={{ color: props.type?.color }}>
+        {props.type?.type}
+      </h1>
+      <p className={style.color} style={{ color: props.type?.color }}>
+        <b>{props.type?.colorName}</b>
+      </p>
+      <Box direction="column" verticalAlignment="center">
+        <Icon
+          className={style.arrow}
+          type="arrow-down"
+          color={props.type?.color}
+          size={90}
+        />
+        <Icon
+          className={style.trash}
+          type="trash"
+          color={props.type?.color}
+          size={120}
+        />
+      </Box>
+    </Box>
+  ) : (
+    <Box
+      direction="column"
+      gap={10}
+      verticalAlignment="center"
+      horizontalAlignment="center"
+    >
+      <Icon
+        className={style.trash}
+        type="x-circle"
+        color={"#ff0000"}
+        size={120}
+      />
+      <h1 className={style.color} style={{ color: "#ff0000" }}>
+        Niestety nie udało się rozpoznać typu odpadu
+      </h1>
+      {props.ai && (
+        <p style={{ color: "#666666", fontSize: "20px", fontWeight: "normal" }}>
+          Znalezione dopasowanie: {props.ai}
+        </p>
+      )}
+    </Box>
+  );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -7,13 +7,54 @@ export const modelCtx = createContext(null) as Context<MobileNet | null>;
 
 //0-papier,1-metale i tworzywa sztuczne,2-szkło,3-bio,4-resztkowe
 export const database: DatabaseRecord[] = [
-    { AIname: ["paper"], type: "Papier", color: "#383087", colorName: "Niebieski" },
-    { AIname: ["water bottle", "pill bottle", "measuring cup", "water jug", "hair spray"], type: "Tworzywa sztuczne i Metale", color: "rgb(255,255,0)", colorName: "Żółty" },
-    { AIname: [], type: "Metale", color: "#E31E25", colorName: "Czerwony" },
-    { AIname: ["pop bottle"], type: "Szkło", color: "#009746", colorName: "Zielony" },
-    { AIname: [], type: "Bio", color: "#8F7459", colorName: "Brązowy" },
-    { AIname: ["lighter"], type: "Zmieszane", color: "#2C2928", colorName: "Czarny" },
-    { AIname: ["cellular telephone", "iPod"], type: "PSZOK", color: "#DB3747", colorName: "Czerwony" },
+  {
+    AIname: ["paper"],
+    type: "Papier",
+    color: "#383087",
+    colorName: "Kolor pojemnika: Niebieski",
+  },
+  {
+    AIname: [
+      "water bottle",
+      "pill bottle",
+      "measuring cup",
+      "water jug",
+      "hair spray",
+    ],
+    type: "Kolor pojemnika: Tworzywa sztuczne i Metale",
+    color: "rgb(255,255,0)",
+    colorName: "Żółty",
+  },
+  {
+    AIname: [],
+    type: "Metale",
+    color: "#E31E25",
+    colorName: "Kolor pojemnika: Czerwony",
+  },
+  {
+    AIname: ["pop bottle"],
+    type: "Szkło",
+    color: "#009746",
+    colorName: "Kolor pojemnika: Zielony",
+  },
+  {
+    AIname: [],
+    type: "Bio",
+    color: "#8F7459",
+    colorName: "Kolor pojemnika: Brązowy",
+  },
+  {
+    AIname: ["lighter"],
+    type: "Zmieszane",
+    color: "#2C2928",
+    colorName: "Kolor pojemnika: Czarny",
+  },
+  {
+    AIname: ["cellular telephone", "iPod"],
+    type: "PSZOK",
+    color: "#DB3747",
+    colorName: "Oddaj to punktu PSZOK",
+  },
 ];
 
 // Global constants
@@ -22,14 +63,14 @@ export const accentColor = "#1A77AA";
 
 // Global data structures
 export interface DatabaseRecord {
-    AIname: string[];
-    type: string;
-    color: string;
-    colorName: string;
+  AIname: string[];
+  type: string;
+  color: string;
+  colorName: string;
 }
 export interface VideoData {
-    devices: MediaDeviceInfo[];
-    device: MediaDeviceInfo;
-    stream: MediaStream;
-    setter: Dispatch<SetStateAction<VideoData>>;
+  devices: MediaDeviceInfo[];
+  device: MediaDeviceInfo;
+  stream: MediaStream;
+  setter: Dispatch<SetStateAction<VideoData>>;
 }
