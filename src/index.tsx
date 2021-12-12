ce3fc88c90f5dac68d80943cd434c71ac78b7c7f
GRZ4NA <grzanatv@gmail.com>
2021-12-12 22:51:00 +0100

    change camera on error

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { load as loadModel } from "@tensorflow-models/mobilenet";
 import { Box } from "./components/Box";
 import { Icon } from "./components/Icon";
 import { Video } from "./components/Video";
+import style from "./components/Settings/index.module.scss";
 
 const main = async () => {
     try {
@@ -25,6 +26,10 @@ const main = async () => {
         );
     } catch (e) {
         console.error(e);
+        const devices = await window.navigator.mediaDevices
+            .enumerateDevices()
+            .then((d) => d.filter((d) => d.kind === "videoinput"))
+            .catch(() => []);
         ReactDOM.render(
             <React.StrictMode>
                 <Box verticalAlignment="center" horizontalAlignment="center" gap={3} direction="column">
@@ -34,6 +39,28 @@ const main = async () => {
                         Upewnij się, że strona posiada odpowiednie uprawnienia a urządzenie obsługuje wymagane funkcje. W przypadku niektórych urządzeń wymagane będzie również
                         połączenie HTTPS.
                     </h3>
+                    {devices && devices.length > 1 && (
+                        <label>
+                            Wybierz kamerę
+                            <Box direction="row" gap={5} verticalAlignment="center">
+                                <select
+                                    className={style.cameraSelector}
+                                    defaultValue={window.localStorage.getItem("videoDeviceId") ?? ""}
+                                    onChange={(e) => {
+                                        e.preventDefault();
+                                        window.localStorage.setItem("videoDeviceId", e.target.value);
+                                        window.location.reload();
+                                    }}
+                                >
+                                    {devices.map((d) => (
+                                        <option key={d.deviceId} value={d.deviceId}>
+                                            {d.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </Box>
+                        </label>
+                    )}
                     <p style={{ color: "#666666" }}>{(e as Error).message}</p>
                 </Box>
             </React.StrictMode>,
